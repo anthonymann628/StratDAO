73ea3d84e818ca6888c42afc17b109714cb8bc8b
Anthony Mann <anthony@amopenhouse.com>
2022-01-19 13:31:51 -0500

    Initial Commit

---
+++ snowbank-frontend/src/views/Dashboard/index.tsx
@@ -0,0 +1,186 @@
+import { useSelector } from "react-redux";
+import { Grid, Zoom } from "@material-ui/core";
+import { trim } from "../../helpers";
+import "./dashboard.scss";
+import { Skeleton } from "@material-ui/lab";
+import { IReduxState } from "../../store/slices/state.interface";
+import { IAppSlice } from "../../store/slices/app-slice";
+
+import { useTranslation } from "react-i18next";
+
+function Dashboard() {
+    const { t } = useTranslation();
+
+    const isAppLoading = useSelector<IReduxState, boolean>(state => state.app.loading);
+    const app = useSelector<IReduxState, IAppSlice>(state => state.app);
+
+    const trimmedStakingAPY = trim(102483.58, 2);
+
+    return (
+        <div className="dashboard-view">
+            <div className="dashboard-infos-wrap">
+                <Zoom in={true}>
+                    <Grid container spacing={4}>
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("RiskFreeValue")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 2,
+                                            minimumFractionDigits: 2,
+                                        }).format(app.rfv)
+                                    )}
+                                </p>
+                                {/* <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 2,
+                                            minimumFractionDigits: 2,
+                                        }).format(app.redeemRfv)
+                                    )}
+                                </p> */}
+                            </div>
+                        </Grid>
+                        {/* <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("RiskFreeValueDelta")}</p>
+                                <p className="card-value">{isAppLoading ? <Skeleton width="250px" /> : `${trim(Number(app.deltaMarketPriceRfv), 2)}%`}</p>
+                            </div>
+                        </Grid> */}
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("STRAT Price")}</p>
+                                <p className="card-value">{isAppLoading ? <Skeleton width="100px" /> : `$${trim(app.marketPrice, 2)}`}</p>
+                            </div>
+                        </Grid>
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("MarketCap")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="160px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.marketCap)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid>
+
+                        {/* <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("SupplyStakedTotal")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        `${new Intl.NumberFormat("en-US", {
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.circSupply)}
+                                        /
+                                        ${new Intl.NumberFormat("en-US", {
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.totalSupply)}`
+                                    )}
+                                </p>
+                            </div>
+                        </Grid> */}
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("Total Value Locked")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.stakingTVL)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid>
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("APY")}</p>
+                                <p className="card-value">{isAppLoading ? <Skeleton width="250px" /> : `${new Intl.NumberFormat("en-US").format(Number(trimmedStakingAPY))}%`}</p>
+                            </div>
+                        </Grid>
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("CurrentIndex")}</p>
+                                <p className="card-value">{isAppLoading ? <Skeleton width="250px" /> : `${trim(Number(app.currentIndex), 2)} STRAT`}</p>
+                            </div>
+                        </Grid>
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("TreasuryBalance")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.treasuryBalance)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid>
+
+                        {/* <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("Backing STRAT")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.rfv)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid> */}
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("Runway")}</p>
+                                <p className="card-value">{isAppLoading ? <Skeleton width="250px" /> : `${trim(365, 1)} Days`}</p>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Zoom>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
