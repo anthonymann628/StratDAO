05ff962a2c728953a8544977d26dbdd92ad3c1c8
Anthony Mann <anthony@amopenhouse.com>
2022-01-28 19:18:13 -0500

    Initial Tomb Changes - AM

---
+++ Tomb/src/views/Home/Home.js
@@ -0,0 +1,416 @@
+import React, { useMemo } from 'react';
+import Page from '../../components/Page';
+import HomeImage from '../../assets/img/home.png';
+import CashImage from '../../assets/img/3OMB.svg';
+import Image from 'material-ui-image';
+import styled from 'styled-components';
+import { Alert } from '@material-ui/lab';
+import { createGlobalStyle } from 'styled-components';
+import CountUp from 'react-countup';
+import CardIcon from '../../components/CardIcon';
+import TokenSymbol from '../../components/TokenSymbol';
+import useTombStats from '../../hooks/useTombStats';
+import useLpStats from '../../hooks/useLpStats';
+import useModal from '../../hooks/useModal';
+import useZap from '../../hooks/useZap';
+import useBondStats from '../../hooks/useBondStats';
+import usetShareStats from '../../hooks/usetShareStats';
+import useTotalValueLocked from '../../hooks/useTotalValueLocked';
+import useFantomPrice from '../../hooks/useFantomPrice';
+import { tomb as tombTesting, tShare as tShareTesting } from '../../tomb-finance/deployments/deployments.testing.json';
+import { tomb as tombProd, tShare as tShareProd } from '../../tomb-finance/deployments/deployments.mainnet.json';
+
+import MetamaskFox from '../../assets/img/metamask-fox.svg';
+
+import { Box, Button, Card, CardContent, Grid, Paper } from '@material-ui/core';
+import ZapModal from '../Bank/components/ZapModal';
+
+import { makeStyles } from '@material-ui/core/styles';
+import useTombFinance from '../../hooks/useTombFinance';
+
+const BackgroundImage = createGlobalStyle`
+  body {
+    background-color: var(--black);
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='32' viewBox='0 0 16 32'%3E%3Cg fill='%231D1E1F' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 24h4v2H0v-2zm0 4h6v2H0v-2zm0-8h2v2H0v-2zM0 0h4v2H0V0zm0 4h2v2H0V4zm16 20h-6v2h6v-2zm0 4H8v2h8v-2zm0-8h-4v2h4v-2zm0-20h-6v2h6V0zm0 4h-4v2h4V4zm-2 12h2v2h-2v-2zm0-8h2v2h-2V8zM2 8h10v2H2V8zm0 8h10v2H2v-2zm-2-4h14v2H0v-2zm4-8h6v2H4V4zm0 16h6v2H4v-2zM6 0h2v2H6V0zm0 24h2v2H6v-2z'/%3E%3C/g%3E%3C/svg%3E");
+}
+
+* {
+    border-radius: 0 !important;
+}
+`;
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    [theme.breakpoints.down('415')]: {
+      marginTop: '10px',
+    },
+  },
+}));
+
+const Home = () => {
+  const classes = useStyles();
+  const TVL = useTotalValueLocked();
+  const tombFtmLpStats = useLpStats('TOMB-FTM-LP');
+  const tShareFtmLpStats = useLpStats('TSHARE-FTM-LP');
+  const tombStats = useTombStats();
+  const tShareStats = usetShareStats();
+  const tBondStats = useBondStats();
+  const tombFinance = useTombFinance();
+  const { price: ftmPrice, marketCap: ftmMarketCap } = useFantomPrice();
+
+  let tomb;
+  let tShare;
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    tomb = tombTesting;
+    tShare = tShareTesting;
+  } else {
+    tomb = tombProd;
+    tShare = tShareProd;
+  }
+
+  const buyTombAddress = 'https://spookyswap.finance/swap?outputCurrency=' + tomb.address;
+  const buyTShareAddress = 'https://spookyswap.finance/swap?outputCurrency=' + tShare.address;
+
+  const tombLPStats = useMemo(() => (tombFtmLpStats ? tombFtmLpStats : null), [tombFtmLpStats]);
+  const tshareLPStats = useMemo(() => (tShareFtmLpStats ? tShareFtmLpStats : null), [tShareFtmLpStats]);
+  const tombPriceInDollars = useMemo(
+    () => (tombStats ? Number(tombStats.priceInDollars).toFixed(2) : null),
+    [tombStats],
+  );
+  const tombPriceInFTM = useMemo(() => (tombStats ? Number(tombStats.tokenInFtm).toFixed(4) : null), [tombStats]);
+  const tombCirculatingSupply = useMemo(() => (tombStats ? String(tombStats.circulatingSupply) : null), [tombStats]);
+  const tombTotalSupply = useMemo(() => (tombStats ? String(tombStats.totalSupply) : null), [tombStats]);
+
+  const tSharePriceInDollars = useMemo(
+    () => (tShareStats ? Number(tShareStats.priceInDollars).toFixed(2) : null),
+    [tShareStats],
+  );
+  const tSharePriceInFTM = useMemo(
+    () => (tShareStats ? Number(tShareStats.tokenInFtm).toFixed(4) : null),
+    [tShareStats],
+  );
+  const tShareCirculatingSupply = useMemo(
+    () => (tShareStats ? String(tShareStats.circulatingSupply) : null),
+    [tShareStats],
+  );
+  const tShareTotalSupply = useMemo(() => (tShareStats ? String(tShareStats.totalSupply) : null), [tShareStats]);
+
+  const tBondPriceInDollars = useMemo(
+    () => (tBondStats ? Number(tBondStats.priceInDollars).toFixed(2) : null),
+    [tBondStats],
+  );
+  const tBondPriceInFTM = useMemo(() => (tBondStats ? Number(tBondStats.tokenInFtm).toFixed(4) : null), [tBondStats]);
+  const tBondCirculatingSupply = useMemo(
+    () => (tBondStats ? String(tBondStats.circulatingSupply) : null),
+    [tBondStats],
+  );
+  const tBondTotalSupply = useMemo(() => (tBondStats ? String(tBondStats.totalSupply) : null), [tBondStats]);
+
+  const tombLpZap = useZap({ depositTokenName: 'TOMB-FTM-LP' });
+  const tshareLpZap = useZap({ depositTokenName: 'TSHARE-FTM-LP' });
+
+  const StyledLink = styled.a`
+    font-weight: 700;
+    text-decoration: none;
+    color: var(--accent-light);
+  `;
+
+  const [onPresentTombZap, onDissmissTombZap] = useModal(
+    <ZapModal
+      decimals={18}
+      onConfirm={(zappingToken, tokenName, amount) => {
+        if (Number(amount) <= 0 || isNaN(Number(amount))) return;
+        tombLpZap.onZap(zappingToken, tokenName, amount);
+        onDissmissTombZap();
+      }}
+      tokenName={'TOMB-FTM-LP'}
+    />,
+  );
+
+  const [onPresentTshareZap, onDissmissTshareZap] = useModal(
+    <ZapModal
+      decimals={18}
+      onConfirm={(zappingToken, tokenName, amount) => {
+        if (Number(amount) <= 0 || isNaN(Number(amount))) return;
+        tshareLpZap.onZap(zappingToken, tokenName, amount);
+        onDissmissTshareZap();
+      }}
+      tokenName={'TSHARE-FTM-LP'}
+    />,
+  );
+
+  return (
+    <Page>
+      <BackgroundImage />
+      <Grid container spacing={3}>
+        {/* Logo */}
+        <Grid container item xs={12} sm={4} justify="center">
+          {/* <Paper>xs=6 sm=3</Paper> */}
+          <Image color="none" style={{ width: '300px', paddingTop: '0px' }} src={CashImage} />
+        </Grid>
+        {/* Explanation text */}
+        <Grid item xs={12} sm={8}>
+          <Paper style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <Box p={4}>
+              <h2>Welcome to Ski Lodge Finance</h2>
+              <p>An algorithmic stablecoin on the Avalanc blockchain, pegged to the price of 1 AVAX</p>
+              <p>Ski Lodge utilizes multiple bonding mechanisms at the <StyledLink href="/rebates">SkiDAO</StyledLink> as well as seigniorage.</p>
+              <p>Built on top of <StyledLink target="_blank" href="https://2omb.finance">2omb.finance</StyledLink>, and forked from <StyledLink target="_blank" href="https://tomb.finance">tomb.finance</StyledLink></p>
+              <p>
+                Stake your SKI-WAVAX LP in the <StyledLink href="/farms">Resort</StyledLink> to earn SKIShares rewards.
+                Then stake your earned SKIShares in the <StyledLink href="/boardroom">Lodge</StyledLink> to maximize profits!
+              </p>
+            </Box>
+          </Paper>
+
+
+
+        </Grid>
+
+        {/* <Grid container spacing={3}>
+    <Grid item  xs={12} sm={12} justify="center"  style={{ margin: '12px', display: 'flex' }}>
+            <Alert severity="warning" style={{ backgroundColor: "transparent", border: "1px solid var(--white)" }}>
+              <b>
+      Please visit our <StyledLink target="_blank" href="https://docs.tomb.finance">documentation</StyledLink> before purchasing TOMB or TSHARE!</b>
+            </Alert>
+        </Grid>
+        </Grid> */}
+
+        {/* TVL */}
+        <Grid item xs={12} sm={4}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center">
+              <h2>Total Value Locked</h2>
+              <CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" />
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* Wallet */}
+        <Grid item xs={12} sm={8}>
+          <Card style={{ height: '100%', backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <CardContent align="center">
+              {/* <h2 style={{ marginBottom: '20px' }}>Wallet Balance</h2> */}
+              <Button color="primary" href="/masonry" variant="contained" style={{ marginRight: '10px' }}>
+                Stake Now
+              </Button>
+              <Button href="/cemetery" variant="contained" style={{ marginRight: '10px' }}>
+                Farm Now
+              </Button>
+              <Button
+                color="primary"
+                target="_blank"
+                href={buyTombAddress}
+                variant="contained"
+                style={{ marginRight: '10px' }}
+                className={classes.button}
+              >
+                Buy SKI
+              </Button>
+              <Button variant="contained" target="_blank" href={buyTShareAddress} className={classes.button}>
+                Buy SKIShare
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* TOMB */}
+        <Grid item xs={12} sm={3}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <h2>AVAX</h2>
+              <Box mt={2} style={{ backgroundColor: "transparent !important" }}>
+                <CardIcon style={{ backgroundColor: "transparent !important" }}>
+                  <TokenSymbol symbol="wFTM" style={{ backgroundColor: "transparent !important" }} />
+                </CardIcon>
+              </Box>
+              Current Price
+              <Box>
+                <span style={{ fontSize: '30px' }}>${ftmPrice ? ftmPrice : '-.----'} USD</span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${(ftmMarketCap)} <br />
+                <br />
+                <br />
+                <br />
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* TOMB */}
+        <Grid item xs={12} sm={3}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <h2>SKI</h2>
+              {/* <Button
+                onClick={() => {
+                  tombFinance.watchAssetInMetamask('TOMB');
+                }}
+                color="secondary"
+                variant="outlined"
+                style={{ position: 'absolute', top: '10px', right: '10px', borderColor: "var(--accent-light)" }}
+              >
+                +&nbsp;
+                <img alt="metamask fox" style={{ width: '20px' }} src={MetamaskFox} />
+              </Button> */}
+              <Box mt={2} style={{ backgroundColor: "transparent !important" }}>
+                <CardIcon style={{ backgroundColor: "transparent !important" }}>
+                  <TokenSymbol symbol="TOMB" style={{ backgroundColor: "transparent !important" }} />
+                </CardIcon>
+              </Box>
+              Current Price
+              <Box>
+                <span style={{ fontSize: '30px' }}>{tombPriceInFTM ? tombPriceInFTM : '-.----'} AVAX</span>
+              </Box>
+              <Box>
+                <span style={{ fontSize: '16px', alignContent: 'flex-start' }}>
+                  ${tombPriceInDollars ? tombPriceInDollars : '-.--'}
+                </span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${(tombCirculatingSupply * tombPriceInDollars).toFixed(2)} <br />
+                Circulating Supply: {tombCirculatingSupply} <br />
+                Total Supply: {tombTotalSupply}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* TSHARE */}
+        <Grid item xs={12} sm={3}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <h2>SKIShares</h2>
+              {/* <Button
+                onClick={() => {
+                  tombFinance.watchAssetInMetamask('TSHARE');
+                }}
+                color="secondary"
+                variant="outlined"
+                style={{ position: 'absolute', top: '10px', right: '10px', borderColor: "var(--accent-light)" }}
+              >
+                +&nbsp;
+                <img alt="metamask fox" style={{ width: '20px' }} src={MetamaskFox} />
+              </Button> */}
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="TSHARE" />
+                </CardIcon>
+              </Box>
+              Current Price
+              <Box>
+                <span style={{ fontSize: '30px' }}>{tSharePriceInFTM ? tSharePriceInFTM : '-.----'} AVAX</span>
+              </Box>
+              <Box>
+                <span style={{ fontSize: '16px' }}>${tSharePriceInDollars ? tSharePriceInDollars : '-.--'}</span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${(tShareCirculatingSupply * tSharePriceInDollars).toFixed(2)} <br />
+                Circulating Supply: {tShareCirculatingSupply} <br />
+                Total Supply: {tShareTotalSupply}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* TBOND */}
+        <Grid item xs={12} sm={3}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <h2>SKILift</h2>
+              {/* <Button
+                onClick={() => {
+                  tombFinance.watchAssetInMetamask('TBOND');
+                }}
+                color="secondary"
+                variant="outlined"
+                style={{ position: 'absolute', top: '10px', right: '10px', borderColor: "var(--accent-light)" }}
+              >
+                +&nbsp;
+                <img alt="metamask fox" style={{ width: '20px' }} src={MetamaskFox} />
+              </Button> */}
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="TBOND" />
+                </CardIcon>
+              </Box>
+              Current Price
+              <Box>
+                <span style={{ fontSize: '30px' }}>{tBondPriceInFTM ? tBondPriceInFTM : '-.----'} AVAX</span>
+              </Box>
+              <Box>
+                <span style={{ fontSize: '16px' }}>${tBondPriceInDollars ? tBondPriceInDollars : '-.--'}</span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${(tBondCirculatingSupply * tBondPriceInDollars).toFixed(2)} <br />
+                Circulating Supply: {tBondCirculatingSupply} <br />
+                Total Supply: {tBondTotalSupply}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center">
+              <h2>SKI-WAVAX LP</h2>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="TOMB-FTM-LP" />
+                </CardIcon>
+              </Box>
+              {/*
+              <Box mt={2}>
+                <Button color="primary" disabled={true} onClick={onPresentTombZap} variant="contained">
+                  Zap In
+                </Button>
+              </Box>*/}
+              <Box mt={2}>
+                <span style={{ fontSize: '26px' }}>
+                  {tombLPStats?.tokenAmount ? tombLPStats?.tokenAmount : '-.--'} SKI /{' '}
+                  {tombLPStats?.ftmAmount ? tombLPStats?.ftmAmount : '-.--'} AVAX
+                </span>
+              </Box>
+              <Box>${tombLPStats?.priceOfOne ? tombLPStats.priceOfOne : '-.--'}</Box>
+              <span style={{ fontSize: '12px' }}>
+                Liquidity: ${tombLPStats?.totalLiquidity ? tombLPStats.totalLiquidity : '-.--'} <br />
+                Total supply: {tombLPStats?.totalSupply ? tombLPStats.totalSupply : '-.--'}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
+            <CardContent align="center">
+              <h2>SKIShares-WAVAX LP</h2>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="TSHARE-FTM-LP" />
+                </CardIcon>
+              </Box>
+              {/*<Box mt={2}>
+                <Button color="primary" onClick={onPresentTshareZap} variant="contained">
+                  Zap In
+                </Button>
+            </Box>*/}
+              <Box mt={2}>
+                <span style={{ fontSize: '26px' }}>
+                  {tshareLPStats?.tokenAmount ? tshareLPStats?.tokenAmount : '-.--'} SKIShare /{' '}
+                  {tshareLPStats?.ftmAmount ? tshareLPStats?.ftmAmount : '-.--'} AVAX
+                </span>
+              </Box>
+              <Box>${tshareLPStats?.priceOfOne ? tshareLPStats.priceOfOne : '-.--'}</Box>
+              <span style={{ fontSize: '12px' }}>
+                Liquidity: ${tshareLPStats?.totalLiquidity ? tshareLPStats.totalLiquidity : '-.--'}
+                <br />
+                Total supply: {tshareLPStats?.totalSupply ? tshareLPStats.totalSupply : '-.--'}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Page>
+  );
+};
+
+export default Home;
---
+++ Tomb/src/views/Rebates/Cemetery.js
@@ -0,0 +1,117 @@
+import React, { useMemo } from 'react';
+import { useWallet } from 'use-wallet';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import Bank from '../Bank';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Box, Card, CardContent, Button, Typography, Grid } from '@material-ui/core';
+
+import { Alert } from '@material-ui/lab';
+
+import UnlockWallet from '../../components/UnlockWallet';
+import Page from '../../components/Page';
+import CemeteryCard from './CemeteryCard';
+import { createGlobalStyle } from 'styled-components';
+import useCashPriceInEstimatedTWAP from '../../hooks/useCashPriceInEstimatedTWAP';
+
+import useBanks from '../../hooks/useBanks';
+import useRebateTreasury from "../../hooks/useRebateTreasury"
+
+const BackgroundImage = createGlobalStyle`
+  body {
+    background-color: var(--black);
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='32' viewBox='0 0 16 32'%3E%3Cg fill='%231D1E1F' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 24h4v2H0v-2zm0 4h6v2H0v-2zm0-8h2v2H0v-2zM0 0h4v2H0V0zm0 4h2v2H0V4zm16 20h-6v2h6v-2zm0 4H8v2h8v-2zm0-8h-4v2h4v-2zm0-20h-6v2h6V0zm0 4h-4v2h4V4zm-2 12h2v2h-2v-2zm0-8h2v2h-2V8zM2 8h10v2H2V8zm0 8h10v2H2v-2zm-2-4h14v2H0v-2zm4-8h6v2H4V4zm0 16h6v2H4v-2zM6 0h2v2H6V0zm0 24h2v2H6v-2z'/%3E%3C/g%3E%3C/svg%3E");
+}
+
+* {
+    border-radius: 0 !important;
+    box-shadow: none !important;
+}
+`;
+
+const useStyles = makeStyles((theme) => ({
+  gridItem: {
+    height: '100%',
+    [theme.breakpoints.up('md')]: {
+      height: '90px',
+    },
+  },
+}));
+
+const Cemetery = () => {
+  const classes = useStyles();
+  const [banks] = useBanks();
+  const { path } = useRouteMatch();
+  const { account } = useWallet();
+  const cashStat = useCashPriceInEstimatedTWAP();
+  const scalingFactor = useMemo(() => (cashStat ? Number(cashStat.priceInDollars).toFixed(4) : null), [cashStat]);
+  const activeBanks = banks.filter((bank) => !bank.finished);
+
+  console.log(cashStat)
+
+  const rebateStats = useRebateTreasury()
+  console.log(rebateStats)
+
+  return (
+    <Switch>
+      <Page>
+        <Route exact path={path}>
+          <BackgroundImage />
+          {!!account ? (
+            <>
+              <Typography color="textPrimary" align="center" variant="h3" gutterBottom style={{ marginBottom: '40px' }}>
+                3DAO - Rebates
+              </Typography>
+              <Box mt={2}>
+                <Grid container justify="center" spacing={3}>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent align="center">
+                        <Typography>
+                          3OMB Price <small>(TWAP)</small>
+                        </Typography>
+                        <Typography>{rebateStats.tombPrice.toFixed(3)}</Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent align="center">
+                        <Typography>
+                          Bond Premium
+                        </Typography>
+                        <Typography>{rebateStats.bondPremium.toFixed(3)}%</Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                </Grid>
+              </Box>
+              <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 0).length === 0}>
+                  <Typography color="textPrimary" variant="h4" gutterBottom style={{ marginTop: '35px', marginBottom: '30px' }}>
+                    Bondable Assets
+                  </Typography>
+                  <Grid container spacing={3}>
+                    {activeBanks
+                      .filter((bank) => bank.sectionInUI === 3)
+                      .map((bank) => (
+                        <React.Fragment key={bank.name}>
+                          <CemeteryCard bank={bank} />
+                        </React.Fragment>
+                      ))}
+                  </Grid>
+                </div>
+            </>
+          ) : (
+            <UnlockWallet />
+          )}
+        </Route>
+        <Route path={`${path}/:bankId`}>
+          <BackgroundImage />
+          <Bank />
+        </Route>
+      </Page>
+    </Switch>
+  );
+};
+
+export default Cemetery;
